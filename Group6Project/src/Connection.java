a36edf9fe507ad46446a940e02a78d5ec03cf11d
Manderas Kidane <99901204+kidane321@users.noreply.github.com>
2023-04-11 16:29:26 -0400

    Update Connection.java

--- Group6Project/src/Connection.java
+++ Group6Project/src/Connection.java
@@ -1,1 +1,39 @@
+import java.sql.Connection;
+
+public class Connection{
+
+   public static void main(String[] args){
+
+       try{
+          Class.forname("Oracle.jdbc.driver.oracleDriver");
+
+       } catch (ClassNotFoundException e){
+           
+            e.printStackTrace();
+       }
+               
+        String serverName = "cisvm-oracle.unfcsd.unf.edu";
+        String portNumber = "1521";
+        String sid = "orcl";
+        String url = "jdbc:oracle:thin:@cisvm-oracle.unfcsd.unf.edu:1521:orcl";
+        String username ="G1"
+        String password ="G6Spring2023"
+         try {
+			Connection conn = DriverManager.getConnection(url, username, password);
+			
+	        boolean reachable = conn.isValid(10);// 10 sec
+
+	        if(reachable) {
+	        	
+	        	System.out.println("Sucess");
+	        	conn.close();
+	        }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+        
+	}
+}
+
 
