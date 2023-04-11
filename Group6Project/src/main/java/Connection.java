31a31a04418687993efc2cb401cbc3c304b8b8da
Jordan-Gallivan <jmatt03@gmail.com>
2023-04-11 16:51:46 -0400

    rebuilt the project in MAVEN so it is supported by intelij and eclipse

---
+++ Group6Project/src/main/java/Connection.java
@@ -0,0 +1,42 @@
+//import java.sql.Connection;
+//import java.sql.Drivermanager;
+//import.java.sql.SQLExecption;
+//
+//public class Connection {
+//
+//   public static void main(String[] args) {
+//
+//       try {
+//           Class.forname("Oracle.jdbc.driver.oracleDriver");
+//	 } catch (ClassNotFoundException e){
+//
+//            e.printStackTrace();
+//       }
+//
+//        String serverName = "cisvm-oracle.unfcsd.unf.edu";
+//        String portNumber = "1521";
+//        String sid = "orcl";
+//	  //
+//        String url = "jdbc:oracle:thin:@cisvm-oracle.unfcsd.unf.edu:1521:orcl";
+//        String username ="G6"
+//        String password ="G6Spring2023"
+//         try {
+//
+//		Connection conn = DriverManager.getConnection(url, username, password);
+//
+//	        boolean reachable = conn.isValid(10);// 10 sec
+//
+//	        if(reachable) {
+//
+//	        	System.out.println("Successfully connected");
+//	        	conn.close();
+//	        }
+//		} catch (SQLException e) {
+//
+//			e.printStackTrace();
+//		}
+//
+//	}
+//}
+//
+//
